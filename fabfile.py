218fdaf27a00770b7c8ef203ecd28b80c91f0d8d
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-09-20 15:07:31 +0545

    [master] python built-in decorator

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+
+from fabric.api import local
+
+def hello(name='Fabric!'):
+    print 'Hello {0}' .format(name)
+
+def test():
+    print 'App Testing....'
+    local('./manage.py test fabapp')
+
+def commit():
+    print 'Committing the changes....'
+    local('git add -p && git commit')
+
+def push():
+    print 'Deploying the app....'
+    local('git push')
+
+def prepare_deploy():
+    # test()
+    commit()
+    push()
